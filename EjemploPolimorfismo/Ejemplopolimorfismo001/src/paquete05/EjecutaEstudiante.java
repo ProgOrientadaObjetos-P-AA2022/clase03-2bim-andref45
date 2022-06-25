0e0cd124435c0e2a64aa521ab93460b8587fc1c8
André Flores <oaflores7@utpl.edu.ec>
2022-06-24 22:46:33 -0500

    Cambio

--- EjemploPolimorfismo/Ejemplopolimorfismo001/src/paquete05/EjecutaEstudiante.java
+++ EjemploPolimorfismo/Ejemplopolimorfismo001/src/paquete05/EjecutaEstudiante.java
@@ -1,9 +1,7 @@
-
 package paquete05;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import paquete04.Estudiante;
 
 public class EjecutaEstudiante {
 
@@ -16,7 +14,7 @@ public class EjecutaEstudiante {
         de tipo EstudiantePresencial y EstudianteDistancia
         quiere ingresar.
         
-        */
+         */
         Scanner entrada = new Scanner(System.in);
         String nombresEst;
         String apellidosEst;
@@ -28,26 +26,83 @@ public class EjecutaEstudiante {
         int numeroAsigs;
         int tipoEstudiante;
         String continuar;
-        int contador;
+        int contador = 0;
         ArrayList<Estudiante> estudiantes = new ArrayList<>();
-        
+        boolean bandera = true;
+
         // inicio de solución
-        
-        
-        
-        
-        
-        // ciclo que permite comprobar el polimorfismo
-        // este código no debe ser modificado.
+        while (bandera) {
+
+            System.out.println("Tipo de Estudiante a ingresar\n"
+                    + "Ingrese (1) para Estudiante Presencial\n"
+                    + "Ingrese (2) para Estudiante Distancia");
+            tipoEstudiante = entrada.nextInt();
+            entrada.nextLine();
+            System.out.println("Ingrese los nombres del estudiante");
+            nombresEst = entrada.nextLine();
+            System.out.println("Ingrese los apellidos del estudiante");
+            apellidosEst = entrada.nextLine();
+            System.out.println("Ingrese la identificación del estudiante");
+            identificacionEst = entrada.nextLine();
+            System.out.println("Ingrese la edad del estudiante");
+            edadEst = entrada.nextInt();
+
+            if (tipoEstudiante == 1) {
+
+                EstudiantePresencial estudianteP = new EstudiantePresencial();
+                System.out.println("Ingrese el número de créditos");
+                numeroCreds = entrada.nextInt();
+                System.out.println("Ingrese el costo de cada crédito");
+                costoCred = entrada.nextDouble();
+
+                estudianteP.establecerNombresEstudiante(nombresEst);
+                estudianteP.establecerApellidoEstudiante(apellidosEst);
+                estudianteP.establecerIdentificacionEstudiante(identificacionEst);
+                estudianteP.establecerEdadEstudiante(edadEst);
+                estudianteP.establecerNumeroCreditos(numeroCreds);
+                estudianteP.establecerCostoCredito(costoCred);
+
+                estudiantes.add(estudianteP);
+
+            } else {
+
+                EstudianteDistancia estudianteD = new EstudianteDistancia();
+
+                System.out.println("Ingrese el número de asignaturas");
+                numeroAsigs = entrada.nextInt();
+                System.out.println("Ingrese el costo de cada cada asignatura");
+                costoAsig = entrada.nextDouble();
+
+                estudianteD.establecerNombresEstudiante(nombresEst);
+                estudianteD.establecerApellidoEstudiante(apellidosEst);
+                estudianteD.establecerIdentificacionEstudiante(identificacionEst);
+                estudianteD.establecerEdadEstudiante(edadEst);
+                estudianteD.establecerNumeroAsginaturas(numeroAsigs);
+                estudianteD.establecerCostoAsignatura(costoAsig);
+
+                estudiantes.add(estudianteD);
+
+            }
+            entrada.nextLine();
+            System.out.println("¿Desea ingresar otro estudiante?");
+            String a = entrada.nextLine();
+            if ("si".equals(a) || "Si".equals(a)) {
+                bandera = true;
+            } else {
+                bandera = false;
+            }
+        }
+
         for (int i = 0; i < estudiantes.size(); i++) {
             // 1.  
             estudiantes.get(i).calcularMatricula();
-            
+
             System.out.printf("Datos Estudiante\n"
-                        + "%s\n",                        
-                  estudiantes.get(i));
-            
+                    + "%s\n",
+                    estudiantes.get(i));
+
         }
+        System.out.println("Ciclo terminado");
     }
 
 }
